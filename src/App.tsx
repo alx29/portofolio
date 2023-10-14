938814ff7a0aa39c5e27102ce94f671e85ea1977
Alex Paraschiv <aparaschiv@adobe.com>
2023-10-15 00:29:29 +0300

    solved image problem

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Github from './icons/Github';
+
+function App() {
+
+  return (
+    <div className='App'>
+      <Github></Github>
+    </div>
+  )
+}
+
+export default App
